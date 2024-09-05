047eb1e7f16b6bf847c586a2c090c9f5ea0cd6c7
Great-Stone <hahohh@gmail.com>
2024-09-05 14:12:24 +0900

    remove terraform cloud stanza

--- policies/terraform/provider.tf
+++ policies/terraform/provider.tf
@@ -1,10 +1,4 @@
 terraform {
-  cloud {
-    organization = "great-stone-biz"
-    workspaces {
-      name = "aws_secret_role_type_check"
-    }
-  }
   required_providers {
     vault = {
       source  = "hashicorp/vault"
